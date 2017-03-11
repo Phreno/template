62710733906613428ec97730b81adbd39d4fdba4
K3rn€lP4n1k <losgoomy@gmail.com>
2017-03-11 07:45:46 +0100

    ajout template sql

---
+++ template.sql
@@ -0,0 +1,27 @@
+-- 
+-- *APPLICATION/MODULE* - *DESCRIPTION RAPIDE*
+--
+
+-- ======================
+-- INFORMATIONS GÉNÉRALES
+-- ======================
+
+-----------------------
+-- MODIFIE LA BASE: *OUI/NON* 
+-----------------------
+
+-- Date........ *yyyy/mm/dd*
+-- Objectifs... *description rapide de l'objectif*
+-- Intention... *description de l'intention*
+-- Etat........ *draft/ready*
+
+------------------
+-- Commentaires --
+------------------
+-- *Toute indication complémentaire*
+
+-- ========
+-- ACTIVITÉ
+-- ========
+
+-- *yyyy/mm/dd* - *VERSION* *BASE* *ENVIRONNEMENT*
