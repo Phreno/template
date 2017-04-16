c4dbb6668dc92fb941badcff347891f586c26373
K3rn€lP4n1k <losgoomy@gmail.com>
2017-04-16 17:16:13 +0200

    bash split base and cartouche

--- bash/base_script.bash
+++ bash/base_script.bash
@@ -1,36 +1,3 @@
-#!/bin/bash
-
-# Développeur ....: K3rn€l_P4n1K
-# Nom ............: @name
-# Description ....: @description
-# Version ........: @version
-# Date ...........: @date
-# Dépendances ....: @dependance
-
-# Fonctionnalité .: @fonctionnalite
-# Intention ......: @intention
-# Remarque .......: @remarque
-
-
-# ===========================
-# Copyright @year K3rn€l P4n1k
-# ===========================
-#
-# This file is part of @application.
-#
-# @application is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# @application is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with @application. If not, see <http://www.gnu.org/licenses/>.
-
 # ------------------
 # VARIABLES GLOBALES
 # ------------------
